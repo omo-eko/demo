322ac07987bd88cb8f5b1a5877e52d44606c7d0e
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-07 11:26:28 +0100

    Added Student and Employer Users with devise'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007093120) do
+ActiveRecord::Schema.define(version: 20141007102457) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,24 @@ ActiveRecord::Schema.define(version: 20141007093120) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "employer_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "employer_users", ["email"], name: "index_employer_users_on_email", unique: true
+  add_index "employer_users", ["reset_password_token"], name: "index_employer_users_on_reset_password_token", unique: true
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -55,6 +73,24 @@ ActiveRecord::Schema.define(version: 20141007093120) do
     t.integer  "buyer_id"
     t.integer  "seller_id"
   end
+
+  create_table "student_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "student_users", ["email"], name: "index_student_users_on_email", unique: true
+  add_index "student_users", ["reset_password_token"], name: "index_student_users_on_reset_password_token", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ test/models/employer_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EmployerUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
